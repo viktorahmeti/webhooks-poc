c16f50dbede8a284de8a127407ef16b2a2789de8
Viktor Ahmeti <viktorahmeti2001@gmail.com>
2024-10-13 00:46:17 +0200

    Efficient Calling of webhooks

--- Client/WebhookHttpClient.cs
+++ Client/WebhookHttpClient.cs
@@ -1,5 +1,4 @@
 using System.Text;
-using System.Text.Json.Serialization;
 using WebHooks.DTO;
 using WebHooks.Mappers;
 using WebHooks.Models;
@@ -8,20 +7,49 @@ namespace WebHooks.Client;
 
 public class WebhookHttpClient : IWebhookHttpClient{
     private readonly IHttpClientFactory _httpClientFactory;
+    private readonly SemaphoreSlim _semaphore;
+    private readonly int MAX_CONCURRENT_CALLS = 200;
 
     public WebhookHttpClient(IHttpClientFactory httpClientFactory){
         _httpClientFactory = httpClientFactory;
+        //Using semaphores, as specified
+        //https://learn.microsoft.com/en-us/dotnet/api/system.threading.semaphoreslim?view=net-8.0
+        _semaphore = new SemaphoreSlim(MAX_CONCURRENT_CALLS, MAX_CONCURRENT_CALLS);
     }
 
     public async void CallWebhooks(Event e){
         EventDTO dto = e.MapToEventDTO();
         string dtoAsJson = Newtonsoft.Json.JsonConvert.SerializeObject(dto);
+        StringContent eventPayload = new StringContent(dtoAsJson, Encoding.UTF8, "application/json");
 
-        Console.WriteLine($"Here is the data for the event: {dto.Name}");
-        foreach (var webhook in e.Webhooks){
-            using (HttpClient client = _httpClientFactory.CreateClient()){
-                await client.PostAsync(webhook.Endpoint, new StringContent(dtoAsJson, Encoding.UTF8, "application/json"));
-            }
+        var tasks = new List<Task>();
+
+        foreach(var webhook in e.Webhooks){
+            await _semaphore.WaitAsync();
+
+            tasks.Add(Task.Run(async () => {
+                try{
+                    HttpClient client = _httpClientFactory.CreateClient("WebhookClient");
+                    var response = await client.PostAsync(webhook.Endpoint, eventPayload);
+                    response.EnsureSuccessStatusCode();
+                }
+                catch(Exception ex){
+                    //handle error
+                    Console.WriteLine(ex.ToString());
+                }
+                finally{
+                    _semaphore.Release();
+                }
+            }));
         }
+
+        await Task.WhenAll(tasks);
+
+        // Basic one-by-one implementation
+        // foreach (var webhook in e.Webhooks){
+        //     using (HttpClient client = _httpClientFactory.CreateClient()){
+        //         await client.PostAsync(webhook.Endpoint, new StringContent(dtoAsJson, Encoding.UTF8, "application/json"));
+        //     }
+        // }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Http.Resilience;
+using Polly;
 using WebHooks.Client;
 using WebHooks.Database;
 using WebHooks.Services;
@@ -15,7 +17,35 @@ builder.Services.AddDbContext<WebhookServiceContext>(options => {
     options.UseSqlite(builder.Configuration.GetConnectionString("WebApiDatabase"));
 });
 
-builder.Services.AddHttpClient();
+
+//configure HTTP Client as per guidelines: https://learn.microsoft.com/en-us/dotnet/fundamentals/networking/http/httpclient-guidelines
+var retryPipeline = new ResiliencePipelineBuilder<HttpResponseMessage>()
+    .AddRetry(new HttpRetryStrategyOptions
+    {
+        BackoffType = DelayBackoffType.Exponential,
+        MaxRetryAttempts = 3
+    })
+    .Build();
+
+var socketHandler = new SocketsHttpHandler
+{
+    MaxConnectionsPerServer = 1000,
+    PooledConnectionLifetime = TimeSpan.FromMinutes(15),
+    PooledConnectionIdleTimeout = TimeSpan.FromMinutes(1)
+};
+
+#pragma warning disable EXTEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+var resilienceHandler = new ResilienceHandler(retryPipeline)
+{
+    InnerHandler = socketHandler,
+};
+#pragma warning restore EXTEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+builder.Services.AddHttpClient("WebhookClient")
+                .ConfigurePrimaryHttpMessageHandler(() => resilienceHandler);
+
+//Finish configuring HTTP Client
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
